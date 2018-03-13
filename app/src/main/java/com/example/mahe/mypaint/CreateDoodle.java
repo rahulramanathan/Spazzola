86bc15ec82aceab654d2a592214df8164a591c15
Rahul Ramanathan <rrahul1997@gmail.com>
2018-03-13 19:26:24 +0530

    Midproject ready
    Splash Screen Works. One Colour and stroke width only.

---
+++ app/src/main/java/com/example/mahe/mypaint/CreateDoodle.java
@@ -0,0 +1,53 @@
+package com.example.mahe.mypaint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class CreateDoodle extends Activity {
+
+    private PaintView paintView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_doodle);
+        Intent intent = getIntent();
+        String s=intent.getStringExtra("hi");
+        paintView = findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
